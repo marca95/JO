869a114907bbbe513e712aa2e36fa854f90db5ab
marca95 <pierre.majerus@outlook.be>
2024-12-13 14:58:32 +0100

    upgrade mariadb

--- jo/manage.py
+++ jo/manage.py
@@ -2,6 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from django.core.management import execute_from_command_line
 
 
 def main():
@@ -19,4 +20,6 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jo.settings.local')
+    # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jo.settings.production')
+    execute_from_command_line(sys.argv)
