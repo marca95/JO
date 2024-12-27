c669af234550525e3aca5e5f7afe85302d235396
marca95 <pierre.majerus@outlook.be>
2024-12-27 16:03:54 +0100

    update header

--- sports/static/js/header.js
+++ sports/static/js/header.js
@@ -2,10 +2,8 @@
 document.addEventListener("DOMContentLoaded", () => {
   const responsiveMenu = document.querySelector(".burger");
   const navLinks = document.querySelector(".nav-links");
-  const navBar = document.querySelector(".navbar");
 
   responsiveMenu.addEventListener("click", () => {
     navLinks.classList.toggle("menu-responsive");
-    navBar.classList.toggle("menu-show");
   });
 });
