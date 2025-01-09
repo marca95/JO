86374f56745236be15d82b6ba14345c7569e6e79
marca95 <pierre.majerus@outlook.be>
2025-01-09 11:31:35 +0100

    continue design event

--- sports/static/js/evenements.js
+++ sports/static/js/evenements.js
@@ -1,82 +1,117 @@
 document.addEventListener("DOMContentLoaded", function () {
   const sportLinks = document.querySelectorAll(".sport-link");
-  const firstSportId = sportLinks[0].dataset.sportId;
+  const eventsContainer = document.getElementById("sport-details");
 
+  const firstSportId = sportLinks[0].dataset.sportId;
   loadEvents(firstSportId);
 
   function loadEvents(sportId) {
     fetch(`/sport/${sportId}/events/`)
-      .then((response) => response.json())
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Erreur réseau lors de la récupération des données.");
+        }
+        return response.json();
+      })
       .then((data) => {
-        const eventsContainer = document.getElementById("sport-details");
-        eventsContainer.innerHTML = "";
+        const backgroundBlur =
+          eventsContainer.querySelector(".background-blur");
+        const contentContainer = eventsContainer.querySelector(".content");
+
+        contentContainer.innerHTML = "";
 
         const sportImageUrl = data.sport_image_url;
         if (sportImageUrl) {
-          eventsContainer.style.backgroundImage = `url(${sportImageUrl})`;
-          eventsContainer.style.backgroundSize = "cover";
-          eventsContainer.style.backgroundPosition = "center";
-          // eventsContainer.style.filter = "blur(4px)";
+          backgroundBlur.style.backgroundImage = `url(${sportImageUrl})`;
+          backgroundBlur.style.backgroundSize = "cover";
+          backgroundBlur.style.backgroundPosition = "center";
+        } else {
+          backgroundBlur.style.backgroundImage = "none";
         }
 
-        data.events.forEach((event) => {
+        data.events.forEach((event, index) => {
           const eventElement = document.createElement("div");
+          eventElement.classList.add(
+            "col-12",
+            "col-xl-6",
+            "layout_card",
+            "mb-4"
+          );
 
-          const eventDate = event.date;
-          const eventHour = event.hour;
-
-          let nationsHtml = "";
-          if (event.nations && event.nations.length > 0) {
-            event.nations.forEach((nation) => {
-              nationsHtml += `<span>${nation.name} (${nation.nickname})</span>`;
-              if (nation.image_url) {
-                nationsHtml += `<img src="${nation.image_url}" alt="${nation.name}" width="50">`;
-              }
-            });
-          } else if (event.players && event.players.length > 0) {
-            if (event.players.length > 4) {
-              nationsHtml = `<span>${event.players.length} sportifs participants</span>
-              <p>Stade: ${event.stadium.name}, Heure: ${event.hour}, Lieu: ${event.stadium.address}</p>`;
-            } else {
-              event.players.forEach((player) => {
-                nationsHtml += `<span>${player.first_name} ${player.last_name}</span>`;
-                if (player.image_url) {
-                  nationsHtml += `<img src="${player.image_url}" alt="${player.first_name} ${player.last_name}" title="${player.first_name} ${player.last_name}" width="50">`;
-                }
-              });
-            }
-          } else {
-            nationsHtml = `<span>Pas encore désigné.</span>`;
-          }
+          const nationsHtml = generateNationsHtml(event);
 
           eventElement.innerHTML = `
-            <h3>${eventDate} à ${eventHour}</h3>
-            <p><strong>${event.stadium.name} à ${
+            <div class="d-flex flex-column align-items-center toto">
+              <div class="opposition d-flex justify-content-between align-items-center w-100">
+                ${nationsHtml}
+              </div>
+              <p class="fs-5 fst-italic fw-bolder">${event.date} à ${
+            event.hour
+          }</p>
+              <p class="fs-5 fst-italic fw-bolder">${event.stadium.name} à ${
             event.stadium.address
-          }</strong></p>
-            <p>Opposants : ${nationsHtml}</p>
-            <p>
-            ${
-              event.stadium.available_space === 0
-                ? "Plus de tickets disponibles"
-                : `Il reste ${event.stadium.available_space} places disponibles. 
-                <a href="/ticket/${event.id}" class="btn btn-primary">Réserver maintenant</a>`
-            }
-          </p>
+          }</p>
+              <p class="fs-5 fst-italic fw-bolder">${
+                event.stadium.available_space === 0
+                  ? "Plus de tickets disponibles"
+                  : `Il reste ${event.stadium.available_space} places disponibles. 
+                    <p class="centr_button"><a href="/ticket/${event.id}" class="button">Réserver maintenant</a></p>`
+              }
+              </p>
+            </div>
           `;
 
-          eventsContainer.appendChild(eventElement);
+          contentContainer.appendChild(eventElement);
+
+          if (
+            index === data.events.length - 1 &&
+            data.events.length % 2 !== 0
+          ) {
+            eventElement.classList.add("mx-auto");
+          }
         });
       })
-      .catch((error) => console.error("Error fetching event data:", error));
+      .catch((error) =>
+        console.error("Erreur lors du chargement des événements:", error)
+      );
+  }
+
+  function generateNationsHtml(event) {
+    let nationsHtml = "";
+
+    if (event.nations && event.nations.length > 0) {
+      nationsHtml = event.nations
+        .map((nation) => {
+          return `<div class="d-flex flex-column align-items-center text-center">
+              <img src="${nation.image_url}" alt="${nation.name}" class="flag-img" style="max-height: 100px; max-width: 160px">
+              <span class="fs-5 fw-bolder mt-2">${nation.name}</span>
+            </div>`;
+        })
+        .join(" / ");
+    } else if (event.players && event.players.length > 0) {
+      if (event.players.length > 4) {
+        nationsHtml = `<span>${event.players.length} sportifs participants</span>`;
+      } else {
+        nationsHtml = event.players
+          .map((player) => {
+            return `<div class="d-flex flex-column align-items-center text-center">
+                <img src="${player.image_url}" alt="${player.first_name} ${player.last_name}" class="player-img" style="max-height: 100px;">
+                <span class="fs-5 fw-bolder mt-2">${player.first_name} ${player.last_name}</span>
+              </div>`;
+          })
+          .join(" / ");
+      }
+    } else {
+      nationsHtml = `<span>Pas encore désigné.</span>`;
+    }
+
+    return nationsHtml;
   }
 
   sportLinks.forEach((link) => {
     link.addEventListener("click", function (event) {
       event.preventDefault();
-
       const sportId = this.dataset.sportId;
-
       loadEvents(sportId);
     });
   });
