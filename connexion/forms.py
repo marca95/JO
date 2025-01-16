101d01691fa68d89b2d72ff03af31096f1291199
marca95 <pierre.majerus@outlook.be>
2025-01-16 10:31:04 +0100

    secure policy password

--- connexion/forms.py
+++ connexion/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, SetPasswordForm
 from django.contrib.auth.models import User
+from django.utils.timezone import localtime
+from panier.models import Cart
 
 class UpdateFormSignupUser(UserCreationForm):
     first_name = forms.CharField(max_length=150, required=True, label="Prénom")
@@ -10,6 +12,13 @@ class UpdateFormSignupUser(UserCreationForm):
     password1 = forms.CharField(
         widget=forms.PasswordInput,
         label="Mot de passe",
+        help_text=(
+            'Votre mot de passe doit respecter les règles suivantes :'
+            '<p class="safety_policy">- Contenir au moins 8 caractères.</p>'
+            '<p class="safety_policy">- Contenir au moins un chiffre.</p>'
+            '<p class="safety_policy">- Contenir au moins une lettre majuscule.</p>'
+            '<p class="safety_policy">- Contenir au moins un caractère spécial (?!.@,).</p>'
+        )
     )
     password2 = forms.CharField(
         widget=forms.PasswordInput,
@@ -46,6 +55,11 @@ class UpdateFormSignupUser(UserCreationForm):
         user.is_superuser = False  
         if commit:
             user.save()
+            
+            Cart.objects.create(
+                user=user,
+                first_key=localtime(user.date_joined).strftime('%Y-%m-%d %H:%M:%S')
+            )
         return user
 
 
@@ -68,8 +82,15 @@ class CustomSetPasswordForm(SetPasswordForm):
     new_password1 = forms.CharField(
         label=('Nouveau mot de passe'),
         widget=forms.PasswordInput(),
-        help_text=('Votre mot de passe doit contenir au moins 8 caractères et inclure des chiffres et des lettres.')
+        help_text=(
+            'Votre mot de passe doit respecter les règles suivantes :'
+            '<p class="safety_policy">- Contenir au moins 8 caractères.</p>'
+            '<p class="safety_policy">- Contenir au moins un chiffre.</p>'
+            '<p class="safety_policy">- Contenir au moins une lettre majuscule.</p>'
+            '<p class="safety_policy">- Contenir au moins un caractère spécial (?!.@,).</p>'
+        )
     )
+
     new_password2 = forms.CharField(
         label=('Confirmer le mot de passe'),
         widget=forms.PasswordInput(),
@@ -80,4 +101,14 @@ class CustomSetPasswordForm(SetPasswordForm):
         password1 = self.cleaned_data.get('new_password1')
         if len(password1) < 8:
             raise forms.ValidationError(('Le mot de passe doit comporter au moins 8 caractères.'))
+        
+        special_characters = {'?', '!', '.', '@', ','}
+        if not any(char in special_characters for char in password1):
+            raise forms.ValidationError(('Le mot de passe doit comporter au moins un caractère spécial (?!.@,).'))
+        
+        if not any(char.isdigit() for char in password1):
+            raise forms.ValidationError(('Le mot de passe doit comporter au moins un chiffre.'))
+        
+        if not any(char.isupper() for char in password1):
+            raise forms.ValidationError(('Le mot de passe doit comporter au moins une lettre majuscule.'))
         return password1
--- panier/models.py
+++ panier/models.py
@@ -3,5 +3,5 @@ from django.contrib.auth.models import User
 
 class Cart(models.Model):
   user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='cart') 
-  first_key = models.CharField(max_length=255, unique=True)
-  second_key = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+  first_key = models.CharField(max_length=255)
+  second_key = models.CharField(max_length=255, null=True, blank=True)
