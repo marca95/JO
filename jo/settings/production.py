16f880302566d5807758bec624e8c20aaac845a1
marca95 <pierre.majerus@outlook.be>
2025-02-03 10:53:30 +0100

    update requirements

--- jo/settings/production.py
+++ jo/settings/production.py
@@ -1,28 +1,26 @@
-# A parametrer en production (retirer tous des commentaires)
+from .base import *
+from decouple import config
 
-# from .base import *
+DEBUG = False
+ALLOWED_HOSTS = ['*']
 
-# DEBUG = False
-# ALLOWED_HOSTS = ['votre-site.com']
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
-# STATIC_ROOT = BASE_DIR / "staticfiles"
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'nom_base_production',
+        'USER': 'utilisateur_production',
+        'PASSWORD': 'mot_de_passe_production',
+        'HOST': 'adresse_serveur',
+        'PORT': '3306',
+    }
+}
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'nom_base_production',
-#         'USER': 'utilisateur_production',
-#         'PASSWORD': 'mot_de_passe_production',
-#         'HOST': 'adresse_serveur',
-#         'PORT': '3306',
-#     }
-# }
-
-# Configuration SMTP pour reset password 
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend' 
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 587
-# EMAIL_HOST_USER = 'monzooarcadia@gmail.com'
-# EMAIL_HOST_PASSWORD = 'pbocfkwegsyuhplk'
-# EMAIL_USE_TLS = True
-# EMAIL_USE_SSL = False
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend' 
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = config('EMAIL_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_PASSWORD')
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
