c20cabf86416b3716d00990aa5fab51554003dd8
marca95 <pierre.majerus@outlook.be>
2025-02-05 15:13:30 +0100

    test erreur

--- jo/settings/production.py
+++ jo/settings/production.py
@@ -42,3 +42,22 @@ EMAIL_USE_SSL = False
 
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
