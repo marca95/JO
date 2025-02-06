922ad6cc3a61002a3fc16fd88e7d0f4ee4221ac4
marca95 <pierre.majerus@outlook.be>
2025-02-06 09:48:34 +0100

    add fields player and nation to false

--- sports/admin.py
+++ sports/admin.py
@@ -7,3 +7,9 @@ class EventAdmin(admin.ModelAdmin):
     list_filter = ('stadium', 'sport', 'date')
     search_fields = ('stadium__name', 'sport__name')
     ordering = ('date', 'hour')
+    
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        form.base_fields['nation'].required = False
+        form.base_fields['player'].required = False
+        return form
